82a3b9f4701e90b3ac2655dcfacfaa37b4a5037a
victory <77779116+tmdrn9@users.noreply.github.com>
2022-04-14 14:50:23 +0900

    Update multiTaskLearningNet.py

--- multiTaskLearningNet.py
+++ multiTaskLearningNet.py
@@ -1,28 +1,23 @@
 from torch import nn
 
+def makefc(shared_layer_size,tower_h1,tower_h2,output_size):
+    return nn.Sequential(
+                nn.Linear(shared_layer_size, tower_h1),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Linear(tower_h1, tower_h2),
+                nn.ReLU(),
+                nn.Dropout(),
+                nn.Linear(tower_h2, output_size)
+            )
+
 class MTLnet(nn.Module):
 
     def __init__(self,basemodel,shared_layer_size,tower_h1,tower_h2,output_size1,output_size2):
         super(MTLnet, self).__init__()
         self.sharedlayer = basemodel
-        self.tower1 = nn.Sequential(
-            nn.Linear(shared_layer_size, tower_h1),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(tower_h1, tower_h2),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(tower_h2, output_size1)
-        )
-        self.tower2 = nn.Sequential(
-            nn.Linear(shared_layer_size, tower_h1),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(tower_h1, tower_h2),
-            nn.ReLU(),
-            nn.Dropout(),
-            nn.Linear(tower_h2, output_size2)
-        )        
+        self.tower1 = makefc(shared_layer_size,tower_h1,tower_h2,output_size1)
+        self.tower2 = makefc(shared_layer_size,tower_h1,tower_h2,output_size2)
 
     def forward(self, xxx):
         shared = self.sharedlayer(xxx)
